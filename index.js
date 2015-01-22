35e19457922f443abed4ac516d81109dba48771a
Raul Sann <raulsannarciso@gmail.com>
2015-01-22 11:16:06 +0100

    Dirty error handling on hamlStream

--- index.js
+++ index.js
@@ -17,14 +17,17 @@ module.exports = function(options) {
   // Map each file to this function
   function hamlStream(file, cb) {
     // Remember that contents is ALWAYS a buffer
-    if (file.isNull()) return cb(null, file); // pass along
-    if (file.isStream()) return cb(new util.PluginError('gulp-haml', 'Streaming not supported'));
-
-    var html = compilers[options.compiler].render(file.contents.toString('utf8'), options.compilerOpts);
-    file.path = rext(file.path, options.ext);
-    file.contents = new Buffer(html);
-
-    cb(null, file);
+    try{
+      if (file.isNull()) return cb(null, file); // pass along
+      if (file.isStream()) return cb(new util.PluginError('gulp-haml', 'Streaming not supported')); 
+      var html = compilers[options.compiler].render(file.contents.toString('utf8'), options.compilerOpts);
+      file.path = rext(file.path, options.ext);
+      file.contents = new Buffer(html);
+      cb(null, file);
+    }catch(e){
+      e.hamlStreamError = 'Error creating stream for file: ' + JSON.stringify(file['history']);
+      throw e;
+    }
   }
 
   // Return a stream
